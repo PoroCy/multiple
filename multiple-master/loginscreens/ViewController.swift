0f29c4fec74515eaa2b1378c0c83bbc1c56e1cb3
PoroCy <30639355+PoroCy@users.noreply.github.com>
2017-08-03 20:59:09 -0700

    Add files via upload

---
+++ multiple-master/loginscreens/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  loginscreens
+//
+//  Created by iD Student on 8/3/17.
+//  Copyright © 2017 iD Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func LoginButton(_ sender: Any) {
+    }
+
+    @IBOutlet weak var Label: UILabel!
+    
+    @IBOutlet weak var InsertUsername: UITextField!
+    
+    @IBOutlet weak var InsertPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+       override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ multiple-master/loginscreens/leaguepicViewController.swift
@@ -0,0 +1,13 @@
+//
+//  leaguepicViewController.swift
+//  loginscreens
+//
+//  Created by Teddy Liang on 8/3/17.
+//  Copyright © 2017 iD Student. All rights reserved.
+//
+
+import UIKit
+
+class leaguepicViewController: NSObject {
+
+}
